d804fc6ca7616018d61b701e16b935d8d4e25348
Samuel Bruin <S.bruin-1@student.tudelft.nl>
2025-05-08 20:55:44 +0200

    Remove redundant object loading function

--- world.cpp
+++ world.cpp
@@ -27,71 +27,6 @@ World load_world() {
 	return world;
 }
 
-void load_one_obj(std::vector<tinybvh::bvhvec4> &triangle_soup, std::vector<tinyobj::material_t> &materials, std::vector<int> &mat_ids, std::string &file_path) {
-	// this function is copied from Rafayels original implementation with slight changes
-
-	// Load the OBJ file using tinyobjloader
-	tinyobj::ObjReaderConfig reader_config;
-	reader_config.triangulate = true; // Ensure triangles are created
-	reader_config.vertex_color = false; // Disable vertex colors
-	reader_config.mtl_search_path = ""; // Set the material search path if needed
-	tinyobj::ObjReader reader;
-	if (!reader.ParseFromFile(file_path, reader_config)) {
-		if (!reader.Error().empty()) {
-			std::cerr << "TinyObjReader: " << reader.Error() << std::endl;
-		}
-		exit(1);
-	}
-	if (!reader.Warning().empty()) {
-		std::cerr << "TinyObjReader: " << reader.Warning() << std::endl;
-	}
-
-	int num_starting_mats = materials.size();
-
-	// Access the loaded shapes and materials
-	const auto &shapes = reader.GetShapes();
-	const auto &attrib = reader.GetAttrib();
-
-	const auto &new_mats = reader.GetMaterials();
-
-	for(tinyobj::material_t new_mat : new_mats){
-		materials.push_back(new_mat);
-	}
-	// Iterate through the shapes and extract the triangles
-	for (tinyobj::shape_t shape : shapes){
-		int face_id = 0;
-		size_t index_offset = 0;
-		for (int fv : shape.mesh.num_face_vertices) {
-			if (fv != 3) {
-				std::cerr << "Warning: Non-triangle face found in OBJ file." << std::endl;
-				continue;
-			}
-			int material_id = -1;
-			if (face_id < shape.mesh.material_ids.size()) {
-				material_id = shape.mesh.material_ids[face_id] + num_starting_mats;
-			}
-			mat_ids.push_back(material_id);
-			tinybvh::bvhvec4 triangle[3];
-			for (size_t v = 0; v < fv; v++) {
-				tinyobj::index_t idx = shape.mesh.indices[index_offset + v];
-				triangle[v] = tinybvh::bvhvec4(
-					attrib.vertices[3 * idx.vertex_index + 0],
-					attrib.vertices[3 * idx.vertex_index + 1],
-					attrib.vertices[3 * idx.vertex_index + 2],
-					material_id
-				);
-			}
-
-			triangle_soup.push_back(triangle[0]);
-			triangle_soup.push_back(triangle[1]);
-			triangle_soup.push_back(triangle[2]);
-			index_offset += fv;
-			face_id++;
-		}
-	}
-
-}
-
 void load_one_obj_at(std::vector<tinybvh::bvhvec4>& triangle_soup, std::vector<tinyobj::material_t>& materials, std::vector<int>& mat_ids, std::string& file_path, glm::vec3 position) {
 	// this function is copied from Rafayels original implementation with slight changes
 
@@ -171,8 +106,8 @@ World::World() {
 }
 
 void World::add_obj(std::string file_path, bool is_lights){
-	load_one_obj(triangle_soup, all_materials, all_material_ids, file_path);
-	if(is_lights) load_one_obj(lights, light_materials, light_material_ids , file_path);
+	load_one_obj_at(triangle_soup, all_materials, all_material_ids, file_path, glm::vec3(0.0f));
+	if(is_lights) load_one_obj_at(lights, light_materials, light_material_ids , file_path, glm::vec3(0.0f));
 }
 
 void World::place_obj(std::string file_path, bool is_lights, glm::vec3 position) {
