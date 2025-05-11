e2b0ec0ad9c4c8f730c2101a229638bf88f8fb68
Samuel Bruin <S.bruin-1@student.tudelft.nl>
2025-05-11 19:14:05 +0200

    Change scene to living room

--- constants.hpp
+++ constants.hpp
@@ -1,8 +1,8 @@
 
 
-constexpr auto BASE_LIGHT_INTENSITY = 250000.0f;
+constexpr auto BASE_LIGHT_INTENSITY = 100.0f;
 
-constexpr auto ENABLE_TEXTURES = false;
+constexpr auto ENABLE_TEXTURES = true;
 
 constexpr auto RENDER_FRAME_COUNT = 200;
 constexpr auto RENDER_WIDTH = 1920;
